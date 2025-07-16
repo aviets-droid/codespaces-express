88f08ecd02f84897da99c60f6b504a57245d994c
aviets-droid <aviets@unr.edu>
2025-07-16 23:40:23 +0000

    UI backend shuffling

--- public/client.js
+++ public/client.js
@@ -4,26 +4,34 @@ const whitespace = "\u00A0";
 
 // Poll Server function here
 
-function createTable() {
-    let table = document.createElement('table');
-    let rows = 4;
-    let cols = 4;
-    
-    table.id = "board";
-    for (let i=1; i<=rows; i++) {
-        let row = table.insertRow();
-        for (let j=1; j<=cols; j++) {
-            let cell = row.insertCell();
-            cell.appendChild(document.createTextNode(whitespace));
-        }
-    }
-    document.body.appendChild(table);
-}
-
 function onTableClick() {
     //
 }
 
 function onButtonClick() {
     //
+}
+
+// Create display function (on HTML parse)
+// Gameboard ID = "board"
+// Button ID = "button"
+function createDisplay() {
+    // Game board
+    let table = document.createElement('table');
+    table.id = "board";
+    for (let i=1; i<=boardRows; i++) {
+        let row = table.insertRow();
+        for (let j=1; j<=boardCols; j++) {
+            let cell = row.insertCell();
+            cell.appendChild(document.createTextNode(whitespace));
+            cell.classList.add("cell");
+        }
+    }
+    document.body.appendChild(table);
+
+    // Button
+    let button = document.createElement('button');
+    button.id = "button";
+    button.textContent = "Flip";
+    document.body.appendChild(button);
 }
