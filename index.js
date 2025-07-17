82913063bd03f9ea7555291188e8f0886ec0fd05
aviets-droid <aviets@unr.edu>
2025-07-17 01:14:05 +0000

    Gameboard/server/button stuff

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000 || process.env.PORT
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.PORT;
+const MAXPLAYERS = 2;
 
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
-app.use('/static', express.static(path.join(__dirname, '/public/')))
-app.use(express.json())
+let playersConnected = 0;
+
+app.use('/static', express.static(path.join(__dirname, '/public/')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/public/', 'index.html'));
 })
 
+app.get('/conn', (req, res) => {
+  playersConnected++;
+  let conns = playersConnected;
+  res.status(200).send("Player connected. Total players: " + conns.toString());
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
--- public/client.js
+++ public/client.js
@@ -1,18 +1,83 @@
 const boardRows = 4;
 const boardCols = 4;
 const whitespace = "\u00A0";
+const POLLINTERVAL = 2000;
 
-// Poll Server function here
+let btn = document.getElementById("button");
 
-function onTableClick() {
-    //
+let myChar = "";
+let boardData = [];
+let gameStarted = false;
+let playersConnected = false;
+
+connectToServer();
+setInterval(pollServer, POLLINTERVAL);
+
+async function pollServer() {
+    return;
+}
+
+async function connectToServer() {
+    try {
+        const conns = await fetch('/conns');
+        const conns_response = await conns.text();
+        console.log(conns_response);
+    }
+    catch (error) {
+        console.error("Error retrieving connected users: " + error);
+    }
 }
 
 function onButtonClick() {
-    //
+    let button = document.getElementById("button");
+    let buttontext = button.textContent;
+    console.log(buttontext + " button clicked");
+
+    if (buttontext == "Flip") {
+        button.disabled = true;
+        onFlip();
+    }
 }
 
-// Create display function (on HTML parse)
+async function onFlip() {
+    // Ping server
+}
+
+function onCellClick(event) {
+    // Update cell
+    const targetCell = event.target;
+    console.log("Cell clicked");
+
+    if (!gameStarted || targetCell.classList.contains("filled")) {
+        return;
+    }
+    else {
+        targetCell.textContent = myChar;
+        targetCell.classList.add("filled");
+    }
+
+    // Check for win condition
+}
+
+function clearBoard() {
+    // Clear data
+
+    // Clear display
+    let table = document.getElementById('table');
+    for (let i=1; i<=boardRows; i++) {
+        for (let j=1; i<=boardCols; j++) {
+            let cell = table.rows[i].cells[j];
+            cell.textContent = whitespace;
+        }
+    }
+}
+
+function updateInfoBar(info) {
+    let infobar = document.getElementById("infobar");
+    infobar.textContent = info;
+}
+
+// Create display (called on HTML parsed in index.html)
 // Gameboard ID = "board"
 // Button ID = "button"
 function createDisplay() {
@@ -21,10 +86,13 @@ function createDisplay() {
     table.id = "board";
     for (let i=1; i<=boardRows; i++) {
         let row = table.insertRow();
+        boardData[i] = [];
         for (let j=1; j<=boardCols; j++) {
             let cell = row.insertCell();
             cell.appendChild(document.createTextNode(whitespace));
             cell.classList.add("cell");
+            cell.addEventListener('click', onCellClick);
+            boardData[i][j] = cell;
         }
     }
     document.body.appendChild(table);
@@ -33,5 +101,6 @@ function createDisplay() {
     let button = document.createElement('button');
     button.id = "button";
     button.textContent = "Flip";
+    button.addEventListener('click', onButtonClick);
     document.body.appendChild(button);
 }
